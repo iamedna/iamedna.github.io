4484b8c2f374c16823e4d4991744ed2671bbccdd
edna <ednapeiyulee@gmail.com>
2017-12-10 13:22:40 -0500

    homework- week four

---
+++ class-work/class07/first-javascript/script.js
@@ -0,0 +1,30 @@
+console.log('this is cool');
+
+var product = 10*10;
+
+var quotient = 100/10;
+var difference = 50- 17;
+var sum = 50+17;
+var name = 'Edna' + ' Lee';
+
+console.log(product, quotient, difference, sum, name);
+
+
+function onra () {
+    var city = 'Saigon';
+
+    console.log (`Last Tango In ${city}`)
+}
+
+onra();
+onra();
+onra();
+
+
+var hello = document.getElementById('hello');
+
+hello.style.color = 'tomato';
+
+
+
+
