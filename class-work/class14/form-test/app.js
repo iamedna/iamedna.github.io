5f994ecfdd6482c84b35b04201bd299a08241277
edna <ednapeiyulee@gmail.com>
2018-01-20 13:04:32 -0500

    homework- week seven

---
+++ class-work/class14/form-test/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#myForm').submit(function(event){
+        event.preventDefault();
+
+        console.log('submitted', $('#name-input').val());
+    });
+})
---
+++ final-project/black-and-white-summer/js/bw.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".info").click(function(){
+        $("#info-page").animate({ right: '+=1440px' }, 25000, 'easeInOutSine');
+
+        
+    });
+  
+});
